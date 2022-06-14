a74c2d478755048ea7b16bc9d43e1f6a0525b033
Kahchana19 <kao.kanhchana19@kit.edu.kh>
2022-06-14 23:35:29 +0700

    side bar

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './App.css';
+import Sidebar from './components/Sidebar';
+import AboutUs from './pages/AboutUs';
+import Home from './pages/Home';
+import { Route } from 'react-router';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { Routes } from 'react-router-dom';
+
+function App() {
+  return (
+    <Router>
+      <Sidebar />
+      <Routes>
+        <Route path='/' exact component={Home} />
+        <Route path='/about' component={AboutUs} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/Components/SidebarData.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import * as AiIcons from 'react-icons/ai';
+import * as IoIcons from 'react-icons/io';
+
+export const SidebarData = [
+  {
+    title: 'Home',
+    path: '/',
+    icon: <AiIcons.AiFillHome />,
+    cName: 'nav-text'
+  },
+  {
+    title: 'About Us',
+    path: '/about',
+    icon: <IoIcons.IoIosPaper />,
+    cName: 'nav-text'
+  },
+  
+];

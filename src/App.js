1a445e181ca1930446b0f3e66a6f1ce09fd0d4cb
Theanchhing <taingtheanchhing18@kit.edu.kh>
2022-06-17 16:55:46 +0700

    final fix

--- src/App.js
+++ src/App.js
@@ -1,22 +1,15 @@
+import React from "react";
 import "./App.css";
 import SwitchSample from "./components/switchSelector";
 import Navbar from "./components/Navbar";
-// import Card from "./Components/Card";
 import { BrowserRouter } from "react-router-dom";
+import Navigation from "./page/Navigation";
 // import { BrowserRouter, Routes, Route } from "react-router-dom";
 // import Home from './page/home'
 // import Search from './page/search'
 // import History  from './page/history';
-
-import Navigation from "./page/Navigation";
-
-import React from "react";
-
-import "./App.css";
 // import Sidebar from './Components/Sidebar';
 // import AboutUs from './pages/AboutUs';
-
-// import { BrowserRouter as Router } from 'react-router-dom';
 
 function App() {
   return (
